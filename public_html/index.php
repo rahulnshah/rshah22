783f8b12fd5ee53e5bdf4bd60af0db6a7557cf64
Rahul Shah <rahulnshah3012@gmail.com>
2022-03-18 21:46:48 -0400

    embeded my contirbution chart

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,7 @@
 	<title>Rahul Shah</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	
 	<!-- <link rel= "stylesheet" type = "text/css" href="CSS/style.css">  -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
@@ -40,6 +41,26 @@
 		<div class="row md-5 g-4">
 		</div>
 	</div>
+	<br>
+	<!--github contribution chart-->
+	<!-- Include the library. -->
+<script
+  src="https://unpkg.com/github-calendar@latest/dist/github-calendar.min.js">
+</script>
+
+<!-- Optionally, include the theme (if you don't want to struggle to write the CSS) -->
+<link rel="stylesheet" href="https://unpkg.com/github-calendar@latest/dist/github-calendar-responsive.css"/>
+<!-- Prepare a container for your calendar. -->
+<div class="container">
+	<div class="calendar">
+		<!-- Loading stuff -->
+		Loading the data just for you.
+	</div>
+</div>
+<script>
+    // or enable responsive functionality:
+    GitHubCalendar(".calendar", "rahulnshah", { responsive: true, tooltips: true});
+</script>
 	<script>
 		function showBasics(key, json)
 		{
