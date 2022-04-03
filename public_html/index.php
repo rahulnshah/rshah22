113a0727af1ea1bd8ec01b2cfc80d95164304f23
Rahul Shah <rahulnshah3012@gmail.com>
2022-04-03 15:58:22 -0400

    fixed spelling

--- public_html/index.php
+++ public_html/index.php
@@ -25,7 +25,7 @@
 				<h1>Welcome!</h1>
 				<p>My name is Rahul.  I am an undergraduate student at New Jersey Institute of Technology, where I am
 				pursuing a Bachelor of Science in Computer Science degree.  I enjoy building elegant software through 
-				programming.  Web Developement, Decentralized Applications (DApps), and Internet of Things (IoT) are things that excite me the most in the realm of Computer Science.  On this homepage, you can find links to my personal
+				programming.  Web Development, Decentralized Applications (DApps), and Internet of Things (IoT) are things that excite me the most in the realm of Computer Science.  On this homepage, you can find links to my personal
 				projects and blog posts.</p>
 				<p>If you have any questions or any suggestions about movies, games, or just want to say a friendly
 				"Hello!", you can reach me at <a class="text-decoration-none" href="mailto:rahulnshah24@gmail.com">rahulnshah24@gmail.com</a>.</p>
