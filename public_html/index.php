a0561aa20891d9737046a99a68872468aaa9a170
Rahul Shah <rahulnshah3012@gmail.com>
2022-04-27 21:15:25 -0400

    added icon of myself

--- public_html/index.php
+++ public_html/index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>Rahul Shah</title>
+	<link rel="shortcut icon" href="myPic.ico" type="image/x-icon">
 	<link rel= "stylesheet" type = "text/css" href="CSS/portfolio.css">  
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.28.0/themes/prism.min.css"/>
@@ -21,6 +22,7 @@
 		<p class="h1">Hi. I'm Rahul</p>
 		</blockquote>
 		<p class="text-center">Software Engineer, Leetcoder, Open Source Enthusiast</p>
+		<h3 class="text-center">🚀</h3>
 	</figure>
 	<div class= "container">
 				<h1>Welcome!</h1>
