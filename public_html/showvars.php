6412b7eb567cff254998cc8f13d67881f4b69c42
Rahul Shah <rahulnshah3012@gmail.com>
2022-05-02 19:32:51 -0400

    made the project viewer

---
+++ public_html/showvars.php
@@ -0,0 +1,25 @@
+<?php
+
+$url = "https://api.github.com/user/repos";
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$headers = array(
+   "Accept: application/json",
+   //hide this 
+   "Authorization: token " . getenv("myscriptvar"),
+   "User-Agent: rahulnshah",
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+$resp = curl_exec($curl);
+curl_close($curl);
+echo $resp; //string
+
+?>
+
