d5e6bf7858837fc08bd8d349d5843f653af90fc2
Rahul Shah <rahulnshah3012@gmail.com>
2023-05-30 22:45:07 -0400

    created routes in a separate folder

---
+++ routes/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render("index");
+});
+
+router.get('/my_projects', (req, res, next) => {
+    // run fetch request
+    const url = "https://api.github.com/search/repositories?q=" + encodeURIComponent('portfolio project in:readme user:rahulnshah');
+        
+    // read our json
+    let fetchPromise = fetch(url, {
+        method : "GET",
+        headers : {
+            "Accept" : "application/vnd.github+json",
+            //hide this using ENV var
+            "Authorization" : `Bearer ${process.env.TOKEN}`,
+            "User-Agent" : "rahulnshah"
+        }
+    });
+    
+    fetchPromise
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        res.render("my_projects", {h1_text : "A Few of my creations", all_projects : data});
+    })
+    .catch(err => {
+		next(err);
+	});
+});
+
+module.exports = {router};
