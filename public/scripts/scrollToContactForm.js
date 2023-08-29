129a796178dcf67906e7a29668c48dd60c011e9f
Rahul Shah <rahulnshah3012@gmail.com>
2023-08-29 12:12:13 -0400

    need to fix submit going to 404 page

--- public/scripts/scrollToContactForm.js
+++ public/scripts/scrollToContactForm.js
@@ -1,6 +1,41 @@
 const scrollToContactFormButton = document.getElementById('scrollToContactFormButton');
-const formHeader= document.querySelector('.contact-form-header');
+const formHeader = document.querySelector('.contact-form-header');
+const contactFormSubmitButton = document.getElementById('contactFormButton');
+const contact_form = document.getElementById("contact-form");
 
 scrollToContactFormButton.addEventListener('click', function () {
-    formHeader.scrollIntoView({ behavior: 'smooth' });
+  formHeader.scrollIntoView({ behavior: 'smooth' });
+});
+
+contactFormSubmitButton.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const contact_form_data = new FormData(contact_form);
+  // convert the data in URL encoded data 
+  const contact_data = new URLSearchParams(contact_form_data);
+  const payload = { data: {} };
+  for (const [key, value] of contact_data.entries()) {
+    payload.data.key = value;
+  }
+  const postFetchPromise = fetch("http://localhost:8000/api/email", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: payload
+  });
+
+  postFetchPromise
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then(text => {
+      console.log("contact form msgs shown!");
+    })
+    .catch(error => {
+      console.log(`Could not render contact form msgs: ${error}`);
+    });
+
 });
