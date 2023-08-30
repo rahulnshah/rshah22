f9a5f3f8901da094cca3fe27f0aad1c2e478f684
Rahul Shah <rahulnshah3012@gmail.com>
2023-08-30 14:44:40 -0400

    renamed a class in errors.js

--- errors.js
+++ errors.js
@@ -2,7 +2,7 @@ class ProjectPageError extends Error {
     constructor(message) {
         super(message);
         this.name = this.constructor.name;
-        if (this instanceof ProjectsInaccesibleError) // deciding what type of error it should be
+        if (this instanceof ProjectsInaccessibleError) // deciding what type of error it should be
         {
             this.type = 'project details';
             this.statusCode = 403; // error code for responding to client
@@ -10,7 +10,7 @@ class ProjectPageError extends Error {
     }
 }
 
-class ProjectsInaccesibleError extends ProjectPageError {
+class ProjectsInaccessibleError extends ProjectPageError {
     constructor() {
         super("Projects inaccessible without access token.");
     }
